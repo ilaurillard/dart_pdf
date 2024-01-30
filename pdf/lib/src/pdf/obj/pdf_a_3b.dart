61d96198763f8eb4ecba2bfa82b3398454d1a53a
ilja <ilaurillard@mindworks.de>
2024-01-30 09:56:46 +0100

    attached files

--- pdf/lib/src/pdf/obj/pdf_a_3b.dart
+++ pdf/lib/src/pdf/obj/pdf_a_3b.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:intl/intl.dart';
+import 'package:xml/xml.dart';
 
 import '../document.dart';
 import '../format/array.dart';
@@ -132,7 +133,7 @@ class AttachedFile extends PdfObject<PdfDictStream> {
   void prepare() {
     super.prepare();
 
-    String modDate = DateFormat("yyyyMMddHHmmss").format(DateTime.now());
+    final String modDate = DateFormat("yyyyMMddHHmmss").format(DateTime.now());
     params['/Type'] = const PdfName('/EmbeddedFile');
     params['/Subtype'] = const PdfName('/application/octet-stream');
     params['/Params'] = PdfDict({
@@ -199,5 +200,131 @@ class ColorProfile extends PdfObject<PdfDictStream> {
         '/DestOutputProfile': ref(),
       }),
     ]);
+  }
+}
+
+class PdfaRdf {
+
+  PdfaRdf({
+    this.title,
+    this.author,
+    this.creator,
+    this.subject,
+    this.keywords,
+    this.producer,
+    DateTime? creationDate,
+    this.invoiceRdf = '',
+  }) {
+    this.creationDate = creationDate ?? DateTime.now();
+  }
+  final String? title;
+  final String? author;
+  final String? creator;
+  final String? subject;
+  final String? keywords;
+  final String? producer;
+  late final DateTime creationDate;
+  final String invoiceRdf;
+
+  XmlDocument? create() {
+    String createDate =
+    DateFormat("yyyy-MM-dd'T'HH:mm:ss").format(creationDate);
+    final offset = creationDate.timeZoneOffset;
+    final hours =
+    offset.inHours > 0 ? offset.inHours : 1; // For fixing divide by 0
+    if (!offset.isNegative) {
+      createDate =
+      "$createDate+${offset.inHours.toString().padLeft(2, '0')}:${(offset.inMinutes % (hours * 60)).toString().padLeft(2, '0')}";
+    } else {
+      createDate =
+      "$createDate-${(-offset.inHours).toString().padLeft(2, '0')}:${(offset.inMinutes % (hours * 60)).toString().padLeft(2, '0')}";
+    }
+
+    return XmlDocument.parse('''
+<?xpacket begin="" id="W5M0MpCehiHzreSzNTczkc9d"?>
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+  <rdf:Description rdf:about="" xmlns:pdf="http://ns.adobe.com/pdf/1.3/">
+    <pdf:Producer>$producer</pdf:Producer>
+    <pdf:Keywords>$keywords</pdf:Keywords>
+  </rdf:Description>
+  <rdf:Description rdf:about="" xmlns:xmp="http://ns.adobe.com/xap/1.0/">
+    <xmp:CreateDate>$createDate</xmp:CreateDate>
+    <xmp:CreatorTool>$creator</xmp:CreatorTool>
+  </rdf:Description>
+  <rdf:Description rdf:about="" xmlns:dc="http://purl.org/dc/elements/1.1/">
+    <dc:creator><rdf:Seq><rdf:li>$author</rdf:li></rdf:Seq></dc:creator>
+    <dc:title><rdf:Alt><rdf:li xml:lang="x-default">$title</rdf:li></rdf:Alt></dc:title>
+    <dc:description><rdf:Alt><rdf:li xml:lang="x-default">$subject</rdf:li></rdf:Alt></dc:description>
+  </rdf:Description>
+  <rdf:Description rdf:about="" xmlns:pdfaid="http://www.aiim.org/pdfa/ns/id/">
+    <pdfaid:part>3</pdfaid:part>
+    <pdfaid:conformance>B</pdfaid:conformance>
+  </rdf:Description>
+  
+  $invoiceRdf
+  
+</rdf:RDF>
+<?xpacket end="r"?>
+''');
+  }
+}
+
+class FacturxRdf {
+  String create() {
+    return '''
+    
+<rdf:Description xmlns:fx="urn:factur-x:pdfa:CrossIndustryDocument:invoice:1p0#" rdf:about="">
+  <fx:DocumentType>INVOICE</fx:DocumentType>
+  <fx:DocumentFileName>factur-x.xml</fx:DocumentFileName>
+  <fx:Version>1.0</fx:Version>
+  <fx:ConformanceLevel>BASIC</fx:ConformanceLevel>
+</rdf:Description>
+    
+<rdf:Description xmlns:pdfaExtension="http://www.aiim.org/pdfa/ns/extension/"
+  xmlns:pdfaField="http://www.aiim.org/pdfa/ns/field#"
+  xmlns:pdfaProperty="http://www.aiim.org/pdfa/ns/property#"
+  xmlns:pdfaSchema="http://www.aiim.org/pdfa/ns/schema#"
+  xmlns:pdfaType="http://www.aiim.org/pdfa/ns/type#"
+  rdf:about=""
+>
+  <pdfaExtension:schemas>
+    <rdf:Bag>
+      <rdf:li rdf:parseType="Resource">
+        <pdfaSchema:schema>Factur-X PDFA Extension Schema</pdfaSchema:schema>
+        <pdfaSchema:namespaceURI>urn:factur-x:pdfa:CrossIndustryDocument:invoice:1p0#</pdfaSchema:namespaceURI>
+        <pdfaSchema:prefix>fx</pdfaSchema:prefix>
+        <pdfaSchema:property>
+          <rdf:Seq>
+            <rdf:li rdf:parseType="Resource">
+              <pdfaProperty:name>DocumentFileName</pdfaProperty:name>
+              <pdfaProperty:valueType>Text</pdfaProperty:valueType>
+              <pdfaProperty:category>external</pdfaProperty:category>
+              <pdfaProperty:description>name of the embedded XML invoice file</pdfaProperty:description>
+            </rdf:li>
+              <rdf:li rdf:parseType="Resource">
+              <pdfaProperty:name>DocumentType</pdfaProperty:name>
+              <pdfaProperty:valueType>Text</pdfaProperty:valueType>
+              <pdfaProperty:category>external</pdfaProperty:category>
+              <pdfaProperty:description>INVOICE</pdfaProperty:description>
+            </rdf:li>
+              <rdf:li rdf:parseType="Resource">
+              <pdfaProperty:name>Version</pdfaProperty:name>
+              <pdfaProperty:valueType>Text</pdfaProperty:valueType>
+              <pdfaProperty:category>external</pdfaProperty:category>
+              <pdfaProperty:description>The actual version of the ZUGFeRD data</pdfaProperty:description>
+            </rdf:li>
+              <rdf:li rdf:parseType="Resource">
+              <pdfaProperty:name>ConformanceLevel</pdfaProperty:name>
+              <pdfaProperty:valueType>Text</pdfaProperty:valueType>
+              <pdfaProperty:category>external</pdfaProperty:category>
+              <pdfaProperty:description>The conformance level of the ZUGFeRD data</pdfaProperty:description>
+            </rdf:li>
+          </rdf:Seq>
+        </pdfaSchema:property>
+      </rdf:li>
+    </rdf:Bag>
+  </pdfaExtension:schemas>
+</rdf:Description>
+''';
   }
 }
